a149b40f5f1af1a86835055343b3eabd4b5bb9d7
tombi5 <79921019+tombi5@users.noreply.github.com>
2023-11-15 08:45:38 +0100

    Add project files.

---
+++ rbz_ratespiel/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace rbz_ratespiel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string eingabe;
+            int geraten;
+            int geheim;
+            int versuche = 0;
+            Random zufall = new Random();
+            geheim = zufall.Next(1, 11);
+            do
+            {
+                Console.Write("Geben Sie eine Zahl zwischen 1 und 10 ein: ");
+                eingabe = Console.ReadLine();
+                geraten = Convert.ToInt32(eingabe);
+                if (geraten < geheim)
+                {
+                    Console.WriteLine("Die gesuchte Zahl ist größer.");
+                }
+                else if (geraten > geheim)
+                {
+                    Console.WriteLine("Die gesuchte Zahl ist kleiner.");
+                }
+                versuche++;
+            } while (geraten != geheim && versuche < 3);
+            if (geraten == geheim)
+            {
+                Console.WriteLine("Herzlichen Glückwunsch! Sie haben gewonnen.");
+            }
+            else
+            {
+                Console.WriteLine("Leider haben Sie verloren. Die gesuchte Zahl war {0}.", geheim);
+            }
+            Console.ReadKey();
+        }
+    }
+}
